e6a32e27ecdfa8fc042e4f94f0fbb1719acb2503
BramFam96 <mtbramlett25@gmail.com>
2022-03-12 10:33:40 -0600

    initialized restful api notes

--- 24.2_apis/notes/mapquest_demo/demo.py
+++
@@ -1,5 +0,0 @@
-import requests
-from secret import mq_key
-
-res = requests.get('http://www.mapquestapi.com/geocoding/v1/address',
-                params = {'key': mq_key, 'location': 'Denver, CO'})
