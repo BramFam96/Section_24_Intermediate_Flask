17b8055814ff4008b8b86f1a128378e908e10265
BramFam96 <mtbramlett25@gmail.com>
2022-03-12 10:10:04 -0600

    fixed testing issue

--- 24.1_WTForms/demo/flask-wtforms-demo/app.py
+++ 24.1_WTForms/demo/flask-wtforms-demo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, render_template
+from flask import Flask, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
